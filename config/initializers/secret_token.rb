633dfdd8d6dba71a918656525a678f5d843966ce
Philipp Knodel <philknodel@googlemail.com>
2013-05-07 22:05:44 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codekitch::Application.config.secret_token = 'fa3e6dc6db593a8995fb9c6c8d869757ae71146eb08bddbe0d13df405df38842302e14905267a649b9c4090f19f121ae0d1c8e1137944dcd5ca862a889efc70a'
